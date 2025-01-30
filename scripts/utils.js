c9dfca42f65112bc1963c8390120b2f690ecf895
itLinkee <emmanuel.p@linkee.co>
2025-01-30 16:49:13 +0100

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,3 +2,7 @@ function clearMarkers() {
     markers.forEach(m => m.setMap(null));
     markers = [];
 }
+
+function getNumberedIcon(number) {
+    return `https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=${number}|FF0000|FFFFFF`;
+}
